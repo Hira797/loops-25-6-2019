849ed922cdb02391202ee32b242e2044089026a3
Hira797 <48818522+Hira797@users.noreply.github.com>
2019-06-25 15:39:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+ for (//kitna chalana hai
+    let i = 0; i < 100; i++) {
+    // kya chalana hai
+console.log("clap!");
+}
+// for 1 to 100
+for (let i = 0; i < 100; i++)
+{
+    console.log(i+1);
+}
+//  for reverse print
+for (let i = 100; i > 0; i--){
+    console.log(i);
+}
