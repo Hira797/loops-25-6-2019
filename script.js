6a543d180d77f518807a8252cca60158e1ee315b
Hira797 <48818522+Hira797@users.noreply.github.com>
2019-06-25 16:02:54 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,11 +4,19 @@
 console.log("clap!");
 }
 // for 1 to 100
-for (let i = 0; i < 100; i++)
-{
-    console.log(i+1);
-}
+// for (let i = 0; i < 100; i++)
+// {
+//     console.log(i+1);
+// }
 //  for reverse print
-for (let i = 100; i > 0; i--){
-    console.log(i);
+// for (let i = 100; i > 0; i--){
+//     console.log(i);
+// }
+function newFunction(){
+    let val1 = document.querySelector('#inp1').value;
+    let val2 = document.querySelector('#inp2').value; 
+console.log(val1 +","+val2);
+for (let h = val1; h < val2; h++){
+    console.log(h);
+}
 }
